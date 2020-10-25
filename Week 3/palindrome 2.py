facc48912f3297c5de267baa1488a85eefb7a80b
Alice H <71669806+aliceeli@users.noreply.github.com>
2020-10-25 17:01:25 +0000

    Update palindrome 2.py

--- Week 3/palindrome 2.py
+++ Week 3/palindrome 2.py
@@ -1,9 +1,7 @@
 palindrome = input("""Please enter a word
 I will tell you if it is a palindrome: """)
 
-palindrome2 = palindrome[::-1]
-
-if palindrome == palindrome2:
+if palindrome == palindrome[::-1]:
     print("That's a palindrome!")
 else:
     print("No, sorry. Try again.")
